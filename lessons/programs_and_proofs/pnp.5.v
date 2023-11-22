3786e58b0b03ab98b709f0e6b4b3d6b41fcdee93
lindvv <lindvv@yandex.ru>
2023-11-22 05:33:27 +0300

    add ExistsUnique proofs

--- lessons/programs_and_proofs/pnp.5.v
+++ lessons/programs_and_proofs/pnp.5.v
@@ -126,7 +126,97 @@ all: done.
 Qed.
 
 
+Definition foo (x y : nat) := if x == y then 1 else 0.
+
+Goal forall x y, x = y -> foo x y = 1.
+Proof.
+rewrite /foo.
+move => x y.
+move /eqP.
+move => ->.
+done.
+Qed.
 
 
+Definition uniqueBothArgs (A: Type) (P: A -> A -> Prop) := exists x, unique (fun x => exists y, unique (P x) y) x.
+Definition unique1stArg (A: Type) (P: A -> A -> Prop) := exists x, unique (fun x => exists y, P x y) x.
+Definition unique2ndArg (A: Type) (P: A -> A -> Prop) := exists x, exists y, unique (P x) y.
+
+Lemma ExistsUniqueMeansExists: forall A (P: A -> Prop), (exists x, unique P x) -> (exists x, P x).
+Proof.
+move => A P H.
+destruct H as [y H].
+exists y.
+apply H.
+Qed.
+
+Theorem ExistsUnique1 : forall (A: Type) (P: A -> A -> Prop), unique1stArg P /\ unique2ndArg P -> uniqueBothArgs P.
+Proof.
+move => A P.
+case => H1 H2.
+destruct H1 as [x1 [_ noOtherX1]].
+destruct H2 as [x2 [y2 [H noOtherY2]]].
+
+exists x2.
+split.
+by exists y2.
+
+
+assert (Hx: exists y : A, P x2 y) by (exists y2; done).
+apply (noOtherX1 x2) in Hx.
+
+rewrite -Hx.
+
+move => x H0.
+apply ExistsUniqueMeansExists in H0.
+auto.
+Qed.
+
+
+From Coq Require Import Nat Arith Lia.
+
+Lemma Counterexample: forall A (P: A -> Prop), (exists (y: A), ~ P y) -> (~ forall (x: A), P x).
+Proof.
+move => A P [y S] T.
+exact (S (T y)).
+Qed.
+
+Theorem ExistsUnique2 : ~ (forall (A: Type) (P: A -> A -> Prop), uniqueBothArgs P -> unique1stArg P /\ unique2ndArg P).
+Proof.
+apply Counterexample.
+exists nat.
+apply Counterexample.
+exists (fun (x y : nat) => (0 = x) <-> (0 = y)).
+cut (uniqueBothArgs (fun x y : nat => (0 = x) <-> (0 = y))).
+move => H1 H2.
+apply H2 in H1; clear H2.
+destruct H1 as [A B].
+destruct A.
+destruct H.
+move : (H0 0) => H1.
+move : (H0 1) => H2.
+assert (T1: exists y : nat, (0 = 0) <-> (0 = y)) by (exists 0; lia).
+assert (T2: exists y : nat, (0 = 1) <-> (0 = y)) by (exists 1; lia).
+apply H1 in T1.
+apply H2 in T2.
+lia.
+
+exists 0.
+split.
+exists 0.
+split.
++ done.
++ move => x H.
+  rewrite -H.
+  done.
+move => x [y H].
+destruct H.
+induction y; induction x; try auto.
++ assert (H2: (0 = x.+1) <-> (0 = x.+1)) by lia.
+  lia.
++ assert (H2: (0 = x.+1) <-> (0 = y.+2)) by lia.
+  apply (H0 (y.+2)) in H2.
+  lia.
+Qed.
 
 
